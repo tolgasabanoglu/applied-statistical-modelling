98dad8be1070d5210580b676380ced21a340761f
tolgasabanoglu <77193923+tolgasabanoglu@users.noreply.github.com>
2024-02-11 19:36:31 +0100

    Update model_comparison.R

--- model_comparison.R
+++ model_comparison.R
@@ -1,17 +1,3 @@
----
-title: "Assignment 5"
-author: "Tolga Şabanoğlu"
-date: "2023-05-22"
-output:
-  html_document: default
-  pdf_document: default
----
-
-
-__5H4. Here is an open practice problem to engage your imagination. In the divorce date, States in the southern United States have many of the highest divorce rates. Add the South indicator variable to the analysis. First, draw one or more DAGs that represent your ideas for how Southern American culture might influence any of the other three variables (D, M or A). Then list the testable implications of your DAGs, if there are any, and fit one or more models to evaluate the implications. What do you think the influence of “Southerness” is?__
-
-
-```{r, Warning= FALSE, message = FALSE}
 # Load the required libraries
 library(brms)
 library(ggplot2)
@@ -22,12 +8,13 @@ library(dagitty)
 library(dplyr)
 library(rethinking)
 
-
+# Define DAG structure
 dag <-
   tibble(name = c("S", "A", "M", "D"),
          x = c(1, 1, 2, 3),
          y = c(3, 1, 2, 1))
 
+# Plot the DAG
 dagify(D ~ A + M,
        M ~ A + S,
        A ~ S,
@@ -39,8 +26,10 @@ dagify(D ~ A + M,
                                               type = "closed")) +
   theme_void()
 
+# Load WaffleDivorce data
 data("WaffleDivorce")
 
+# Prepare data for the model
 south_divorce <- WaffleDivorce %>%
   as_tibble() %>%
   select(D = Divorce,
@@ -50,17 +39,15 @@ south_divorce <- WaffleDivorce %>%
   drop_na(everything()) %>%
   mutate(across(where(is.double), standardize))
 
+# Fit the model
 model <- brm(D ~ A + M + S, data = south_divorce, family = gaussian,
             prior = c(prior(normal(0, 0.2), class = Intercept),
                       prior(normal(0, 0.5), class = b),
                       prior(exponential(1), class = sigma)),
             iter = 4000, warmup = 2000, chains = 4, cores = 4, seed = 1234)
-            
 
+# Display model summary
 summary(model)
 
+# Plot model diagnostics
 plot(model)
-
-```
-
-The effect of S is approximately 0.37, indicating that southerness is not very strong in relation to the other variables.
